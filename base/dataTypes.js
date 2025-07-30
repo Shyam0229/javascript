9559e2f64649e23552ac39f5bea5b58b10b22cba
Shyam0229 <519sujoymalik@gmail.com>
2025-07-30 13:06:23 +0000

    datatypes

---
+++ base/dataTypes.js
@@ -0,0 +1,29 @@
+"use strict" //treat all js code as newwer version
+//alert("hello") // we use nodejs ,not browsing
+console.log(3+3)
+console.log('hay');
+
+
+let name="shyam"
+let age =20
+let login=true
+
+  // PREMITIVE
+// number=>2 to power 53
+// bigInt = bignumber store like stock market ,facebok etc
+// string =>""
+//boolean=ture/false
+// null =standalone value //empty
+// undefine = value not assign but variable are declared
+// symbols => unique
+
+  // NON PRIMITIVE
+//object
+
+
+console.log(typeof "shyam");
+console.log(typeof age);
+
+
+console.log(typeof undefined);
+console.log(typeof null);
---
+++ base/variable.js
@@ -0,0 +1,14 @@
+const accountId=123
+let accountEmail="xyz@gamil.com"
+var accountPassword="12345"
+accountCity="india"
+let accountState
+
+// accountId =1
+accountEmail="abc@gmail.com"
+accountPassword="789"
+accountCity="japan"
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
