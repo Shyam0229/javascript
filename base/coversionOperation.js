9db3ecc5cb909cfb0b372ad88df77e6d76fe9791
Shyam0229 <519sujoymalik@gmail.com>
2025-07-31 12:35:50 +0000

    operation

--- base/coversionOperation.js
+++ base/coversionOperation.js
@@ -29,6 +29,38 @@ console.log(typeof bolleanlogin);
 
 let number = 24
 let Stringnumber=String(number)
-console.log(Stringnumber);
-console.log(typeof Stringnumber);
+/* console.log(Stringnumber);
+console.log(typeof Stringnumber); */
 
+//...............OPERATION.................//
+
+let number1= 3
+let negNumber = -number1
+// console.log(negNumber);
+
+
+let str1 = "hello"
+let str2 = " shyam"
+let str3 = str1 + str2
+/* console.log(str3);
+
+console.log( 1 + 2 );  //3
+console.log( "1" + 2 );  // 12
+console.log( 1 + "2" );  // 12
+console.log( "1" + 2 + 2 ); //122 
+console.log( 1 + 2 + "2" );   // 32
+console.log( 1 + "2" + 2 ); //122
+
+console.log(+true); // 1
+console.log(+"")   // 0
+console.log(true); // true */
+
+
+let num1 , num2 , num3
+ num1 = num2 = num3 = 2 + 2
+
+// num1=num2=num3=4
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
