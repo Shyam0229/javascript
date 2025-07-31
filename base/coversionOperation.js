c7c8805f754b41fbfd4762ee710f1e5f6931a781
Shyam0229 <519sujoymalik@gmail.com>
2025-07-31 11:39:43 +0000

    conversion operation

---
+++ base/coversionOperation.js
@@ -0,0 +1,34 @@
+let score= "33abs"
+
+
+/* console.log(score);
+console.log(typeof score); */
+
+let value= Number(score)
+/* console.log(value);
+console.log(typeof value); */
+
+// "33"=>33 , number to number  ,value is ok
+// "33abc" =>NaN   , not a number ,string to number
+// true => 1 , false => 0
+//null => 0 ,
+//undefine =>NaN 
+
+
+let login = ""
+
+
+let bolleanlogin =Boolean(login)
+/* console.log(bolleanlogin);
+console.log(typeof bolleanlogin);
+ */
+// "1" = > true
+// "0" => false
+//""  => false
+//
+
+let number = 24
+let Stringnumber=String(number)
+console.log(Stringnumber);
+console.log(typeof Stringnumber);
+
