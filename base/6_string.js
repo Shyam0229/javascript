a6cbdf0c2b3c7bbb0af4bba62c38c2d2159140ba
Shyam0229 <519sujoymalik@gmail.com>
2025-08-01 13:09:49 +0000

    string concepts

---
+++ base/6_string.js
@@ -0,0 +1,23 @@
+const name = " shyam "
+const value =   50
+
+
+// console.log(name +  value + " kumar malik ");
+
+// console.log(`hello my name is ${name}, my value is ${value} `); 
+// this use for string interpolation
+
+const Name = new String('shyam') // this is a one way to print string
+
+console.log(Name);  //[String: 'shyam']
+
+console.log(Name[0]) // s
+
+console.log(Name.__proto__);  //prtotype is {}-this means object type 
+
+console.log(Name.length); //5
+console.log(Name.toUpperCase()); // all are uper format
+
+
+
+
