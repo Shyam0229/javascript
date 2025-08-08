edcba4640e00b127c17ca2ae167faf6e8c1be5b5
Shyam0229 <519sujoymalik@gmail.com>
2025-08-08 13:23:10 +0000

    controlFlow using

---
+++ control_flow4/one.js
@@ -0,0 +1,82 @@
+// IF
+// checked equqlto and type
+// if (2==="2") {
+//     console.log("this is right");
+    
+// }
+// console.log("wrong..number not same as a stirng");
+
+
+//  let temp = 35
+//  if (temp<50) {
+//     console.log("yes this a true... les than 50");
+    
+//  }
+//  else{
+//      console.log("not a true...");
+
+//  }
+
+//  < , > , <= , >= , != , == , === , !==
+
+
+// const score=100
+// if (score > 20) {
+//     const power="fly"
+//     console.log(`The Power is: ${power}`);
+    
+// }
+// console.log(`The Power is: ${power}`); // this in power - const type so
+        //     not access in out of if scope 
+        // let type
+        //   one type of advantagess
+ 
+// const score=100
+// if (score > 20) {
+//     var power="fly"
+//     console.log(`The Power is: ${power}`);
+    
+// }        
+//  console.log(`The Power is: ${power}`); // this in power - var type so
+        //      access in out of if scope 
+        // one type of disadvantagess
+
+
+
+//   +++++++++++++++++++++++++ implicit +++++++++
+// const rupes = 1000
+// if (rupes > 500) console.log("true"),console.log("test 2"),
+// console.log("test 3");
+// one line excution ,here coma separate multi line and semicolan stop the excution 
+
+
+// const balance = 100
+// if (balance < 500) {
+//       console.log("less than 500");
+      
+// } else  if(balance < 750){
+//     console.log("less than 750");
+// }
+//  else  if(balance < 1000){
+//     console.log("less than 750");
+// }
+//  else{
+//     console.log("less than 1200");
+// }
+
+   
+const userRegister = true
+const password= true
+const loginInFromGoogle= false
+const loginInFromEmail =true
+
+if(userRegister && password && 2==3){
+    console.log("you are login");
+    
+}
+
+if (loginInFromGoogle || loginInFromEmail ||) {
+    console.log("loginInFromGoogle , loginInFromEmail any one chosee  ");
+    
+    
+}
---
+++ control_flow4/truthy.js
@@ -0,0 +1,89 @@
+// const userEmail ="shyam@gmail.com"
+
+// if (userEmail) {
+//     console.log("got a user email ");
+    
+// }else{
+//     console.log("no have a user email ");
+    
+// }
+
+// // useremail are string type ,, so true value ,then go if statement
+
+
+
+
+// const userEmail =""
+
+// if (userEmail) {
+//     console.log("got a user email ");
+    
+// }else{
+//     console.log("no have a user email ");
+    
+// }
+
+// // useremail are empty type ,, so false value ,then go else statement
+
+
+// const userEmail =[]
+
+// if (userEmail) {
+//     console.log("got a user email ");
+    
+// }else{
+//     console.log("no have a user email ");
+    
+// }
+
+// useremail are array type ,, so true value ,then go if statement
+
+
+// falsy value
+// false , 0 , -0 , Bigint 0n , "" , null , undefined , NaN 
+
+// turely value
+// all the rest ,everything else except these 
+// surprised value of turely like --- "0" , " false " , " " , { } , [ ] , function(){} , 
+
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty ");
+    
+    
+// }
+
+// const emptyObj = {}
+
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("empty opject ");
+    
+// }
+
+// false==0 // true
+// false=='' //true
+// 0==''  // true
+
+
+// in database use database to frontend request - null or  undefined checked - request go or not 
+// Nullish Coalescing Operator (??) : Null Undefined   
+
+// let val1; 
+// val1 = 5 ?? 10   // 5
+// val1 = null ?? 10   // 10
+// val1 = undefined ?? 15  // 15
+// val1= null ?? 10 ?? 20  // 10
+// console.log(val1);
+
+
+// TERNARY OPERATOR //
+// condition ? true : false
+
+
+// const price = 100
+//  price <= 80 ? console.log(" less than 80") : console.log("more than 80");
+ 
+ 
+
+
+
