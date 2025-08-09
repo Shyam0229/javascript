c9cdf25d3fbe977f3b88fc1f17dfe003a9c6b7c8
Shyam0229 <519sujoymalik@gmail.com>
2025-08-09 13:16:19 +0000

    do while

---
+++ iteration5/two.js
@@ -0,0 +1,26 @@
+// WHILE AND DO WHILE
+
+
+i=0
+while (i<=10) {
+    // console.log(`value of index is ${i}`);
+    i= i+2
+    
+}
+
+
+
+let myArray = ["flash", "badman", "saktiman" , "spiderman"]
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`value is myarray ${myArray[arr]}`);
+    arr++
+}
+
+
+
+let v=11
+do {
+    console.log(`score is ${v}`);
+    v++
+} while (v <= 10);
