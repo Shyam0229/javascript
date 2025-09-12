ffae7ea28c003772470c58537fe69d187cae78a4
Shyam0229 <519sujoymalik@gmail.com>
2025-09-12 12:08:06 +0000

    project 4

--- home-project/GuessNumber/pro4.js
+++ home-project/GuessNumber/pro4.js
@@ -1,1 +1,43 @@
-console.log( parseInt(Math.random()* 100 +1) );
+const randomNumber=( parseInt(Math.random()* 100 +1) );
+
+const submit =document.querySelector('#subt')
+const userInput =document.querySelector('#guessField')
+const guessesSlot =document.querySelector('.guesses')
+const remaning =document.querySelector('.lastResult')
+const lowOrHi =document.querySelector('.lowOrHi')
+const startOver =document.querySelector('.resultParas')
+
+
+const p = document.createElement('p')
+
+let prevGuess = []
+let numGuess = 1
+
+let playgame = true;
+
+
+
+function valiodateGuess(guess){
+
+}
+
+function checkGuess(guess){
+
+}
+
+function displayGuess(guess){
+
+}
+
+function displayMessage(message){
+
+}
+
+function endGame(){
+
+}
+
+
+function newGame(){
+    
+}
